1c7cde36ed840a98a5d54ab6e600b12bb2438d15
Èric Zapater <ezapaterm@gmail.com>
2019-02-18 22:06:44 +0100

    First HelloWorld controller

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BibliotecaCoreNet.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        //GET:/HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET : /HelloWorld/Welcome
+        public string Welcome(string name, int numtimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}. Numtimes {numtimes}");
+        }
+
+    }
+}
